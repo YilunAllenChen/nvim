8b2eb5c79875c501f0d9cebc7ca652edcb2f2cd6
Allen Chen <alchen@drwholdings.com>
2023-11-05 13:56:27 -0600

    fix nullls

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -16,40 +16,50 @@ return
 {
   'folke/neodev.nvim',
   'neovim/nvim-lspconfig',
-  dependencies = {
-    {
-      'williamboman/mason.nvim',
-      config = function()
-        require("mason").setup()
-      end
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require("neodev").setup()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      local mlsp = require("mason-lspconfig")
+      mlsp.setup {
+        ensure_installed = vim.tbl_keys(servers),
+      }
+      mlsp.setup_handlers {
+        function(server_name)
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+          }
+        end,
+      }
+    end
+  },
+  {
+    'nvim-lua/lsp-status.nvim',
+    config = function()
+      require 'lsp-status'.register_progress()
+    end,
+  },
+  { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
     },
-    {
-      'williamboman/mason-lspconfig.nvim',
-      config = function()
-        require("neodev").setup()
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-        local mlsp = require("mason-lspconfig")
-        mlsp.setup {
-          ensure_installed = vim.tbl_keys(servers),
-        }
-        mlsp.setup_handlers {
-          function(server_name)
-            require('lspconfig')[server_name].setup {
-              capabilities = capabilities,
-              settings = servers[server_name],
-              filetypes = (servers[server_name] or {}).filetypes,
-            }
-          end,
-        }
-      end
+    opts = {
+      ensure_installed = { "prettier", "stylua", "black", "jq", "jsonnetfmt" },
+      handlers = {},
     },
-    {
-      'nvim-lua/lsp-status.nvim',
-      config = function()
-        require 'lsp-status'.register_progress()
-      end,
-    },
-    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
   },
 }
