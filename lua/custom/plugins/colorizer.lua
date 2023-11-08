d0632b9fcc795a6343e02ec5bb2a787c4b4d44a4
Allen Chen <alchen@drwholdings.com>
2023-11-08 16:53:32 -0600

    colorizer don't highlight names

--- lua/custom/plugins/colorizer.lua
+++ lua/custom/plugins/colorizer.lua
@@ -1,7 +1,9 @@
 -- display color codes in color
 return {
   'norcalli/nvim-colorizer.lua',
-  config = function ()
-    require("colorizer").setup()
-  end
+  config = function()
+    require('colorizer').setup({}, {
+      name = false,
+    })
+  end,
 }
