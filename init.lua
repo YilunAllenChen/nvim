d5491a8edf89259d1aa8315f16f9a82496924bcc
YilunAllenChen <allenchenyilun1999@gmail.com>
2023-12-20 20:15:25 -0600

    tab

--- init.lua
+++ init.lua
@@ -28,6 +28,8 @@ local options = {
     showtabline = 2, -- always display tabline
     splitbelow = true, -- splitting a new window below the current one
     splitright = true, -- splitting a new window at the right of the current one
+    shiftwidth = 4,
+    tabstop = 4,
   },
   g = {
     mapleader = ' ',
