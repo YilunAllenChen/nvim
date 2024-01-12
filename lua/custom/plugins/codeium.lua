ab76ce69c82f1904c8c16e87dbe33b02deec0406
Allen Chen <alchen@drwholdings.com>
2024-01-12 06:49:23 -0600

    codeium

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -3,7 +3,7 @@ return {
   tag = '1.4.15',
   event = 'BufEnter',
   config = function()
-    vim.keymap.set('i', '<C-l>', function()
+    vim.keymap.set('i', '<Right>', function()
       return vim.fn['codeium#Accept']()
     end, { expr = true, silent = true })
   end,
