12bd9f8393267911f124cf869a85cb5be8f88f8b
Allen Chen <alchen@drwholdings.com>
2024-04-30 08:12:49 -0500

    /lock devicon

--- lua/custom/plugins/devicon.lua
+++ lua/custom/plugins/devicon.lua
@@ -1,35 +1,35 @@
 -- icons
 return {
-  "nvim-tree/nvim-web-devicons",
-  lazy = true,
-  config = function()
-    require("nvim-web-devicons").setup({
-      override = {
-        zsh = {
-          icon = "",
-          color = "#428850",
-          cterm_color = "65",
-          name = "Zsh"
-        }
-      },
-      color_icons = true,
-      default = true,
-      strict = true,
-      override_by_filename = {
-        [".gitignore"] = {
-          icon = "",
-          color = "#f1502f",
-          name = "Gitignore"
-        }
-      },
-      override_by_extension = {
-        ["log"] = {
-          icon = "",
-          color = "#81e043",
-          name = "Log"
-        }
-      },
-
-    })
-  end
+	'nvim-tree/nvim-web-devicons',
+	commit = '8b2e5ef9eb8a717221bd96cb8422686d65a09ed5',
+	lazy = true,
+	config = function()
+		require('nvim-web-devicons').setup {
+			override = {
+				zsh = {
+					icon = '',
+					color = '#428850',
+					cterm_color = '65',
+					name = 'Zsh',
+				},
+			},
+			color_icons = true,
+			default = true,
+			strict = true,
+			override_by_filename = {
+				['.gitignore'] = {
+					icon = '',
+					color = '#f1502f',
+					name = 'Gitignore',
+				},
+			},
+			override_by_extension = {
+				['log'] = {
+					icon = '',
+					color = '#81e043',
+					name = 'Log',
+				},
+			},
+		}
+	end,
 }
