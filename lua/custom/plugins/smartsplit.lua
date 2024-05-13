c8587c0019d72357d2652cfc9d3e2723a22d12c9
Allen Chen <alchen@drwholdings.com>
2024-05-13 10:41:20 -0500

    /add tree

--- lua/custom/plugins/smartsplit.lua
+++ lua/custom/plugins/smartsplit.lua
@@ -1,3 +1,14 @@
 return {
   'mrjones2014/smart-splits.nvim',
+  config = function()
+    require('smart-splits').setup {
+      ignored_buftypes = {
+        'quickfix',
+        'nofile',
+        'prompt',
+        'terminal',
+		'nvimtree',
+      },
+    }
+  end,
 }
