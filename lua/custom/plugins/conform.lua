8f7f8bb7d1b440e52f6a575d23b521360bff534a
Allen Chen <alchen@drwholdings.com>
2024-10-19 10:34:26 -0500

    /add cpp

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,21 +1,22 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
-  config = function()
-    require('conform').setup {
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform will run multiple formatters sequentially
-        python = { 'isort', 'black' },
-        -- Use a sub-list to run only the first available formatter
-        -- javascript = { { 'prettierd', 'prettier' } },
-        json = { 'prettier' },
-      },
-      format_after_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = 'never',
-      },
-    }
-  end,
+	'stevearc/conform.nvim',
+	opts = {},
+	config = function()
+		require('conform').setup {
+			formatters_by_ft = {
+				cpp = { 'clang-format' },
+				lua = { 'stylua' },
+				-- Conform will run multiple formatters sequentially
+				python = { 'isort', 'black' },
+				-- Use a sub-list to run only the first available formatter
+				-- javascript = { { 'prettierd', 'prettier' } },
+				json = { 'prettier' },
+			},
+			format_after_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = 'never',
+			},
+		}
+	end,
 }
