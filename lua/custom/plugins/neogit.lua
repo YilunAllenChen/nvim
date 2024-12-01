c8df9d8e946fa2f6211bf5871b250a38184e1ca9
Allen Chen <alchen@drwholdings.com>
2024-12-01 16:46:57 -0600

    add drw git

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -1,8 +1,13 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-  },
-  config = true
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+	},
+	config = {
+		git_services = {
+			["git.drwholdings.com"] =
+			"https://git.drwholdings.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+		},
+	}
 }
