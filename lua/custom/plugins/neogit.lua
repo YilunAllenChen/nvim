9240c7a76a4dd90e620a80593ee1c0e306a1901f
Allen Chen <alchen@drwholdings.com>
2024-12-01 20:47:40 -0600

    /add ergonomics improvements for neogit

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -1,13 +1,20 @@
 return {
-	"NeogitOrg/neogit",
-	dependencies = {
-		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
-	},
-	config = {
-		git_services = {
-			["git.drwholdings.com"] =
-			"https://git.drwholdings.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-		},
-	}
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+  },
+  config = {
+    graph_style = 'unicode',
+
+    disable_hint = true,
+
+    use_per_project_settings = false,
+    commit_editor = {
+      kind = 'floating_console',
+    },
+    git_services = {
+      ['git.drwholdings.com'] = 'https://git.drwholdings.com/${owner}/${repository}/compare/${branch_name}?expand=1',
+    },
+  },
 }
