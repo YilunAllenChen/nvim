a796eb9e79b87c360535c835f44ca369fac5a0ab
Allen Chen <alchen@drwholdings.com>
2024-12-01 21:59:29 -0600

    remove unused themes

--- lua/custom/themes/nord.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'AlexvZyl/nordic.nvim',
-  lazy = true,
-  config = function()
-    require('nordic').load()
-  end,
-}
--- lua/custom/themes/onedarker.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'lunarvim/Onedarker.nvim',
-  lazy = true,
-}
