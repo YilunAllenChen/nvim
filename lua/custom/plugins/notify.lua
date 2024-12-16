587651b127349adf418f0504ccd8722c7a0d07b0
Allen Chen <alchen@drwholdings.com>
2024-12-15 21:55:32 -0600

    notify warn

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -2,6 +2,7 @@ return {
   'rcarriga/nvim-notify',
   config = function()
     require('notify').setup {
+      level = 'warn',
       stages = 'slide',
       timeout = 3000,
       render = 'compact',
