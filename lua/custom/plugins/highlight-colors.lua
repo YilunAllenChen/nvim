9b5ca6bfbd7acb3473840567bcea58b0dd4e3476
Allen Chen <alchen@drwholdings.com>
2024-12-28 12:24:19 -0600

    use highlight colors instead of nvim-colorizer
    remove blink setup

---
+++ lua/custom/plugins/highlight-colors.lua
@@ -0,0 +1,11 @@
+-- display color codes in color
+return {
+  'brenoprata10/nvim-highlight-colors',
+  config = function()
+    vim.opt.termguicolors = true
+    require('nvim-highlight-colors').setup {
+      render = 'virtual',
+      enable_tailwind = true,
+    }
+  end,
+}
