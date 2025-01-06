ea41cfa40a325fd031ca251f8f314367e8fb40ca
Allen Chen <alchen@drwholdings.com>
2025-01-06 07:32:16 -0600

    notify

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('notify').setup {
       merge_duplicates = false,
-      level = 'warning',
+      level = vim.log.levels.WARN,
       stages = 'slide',
       timeout = 3000,
       render = 'wrapped-compact',
