5b4ef27bcb449d823fd256c5a04b74f18b2a3390
Allen Chen <alchen@drwholdings.com>
2025-04-18 07:40:11 -0500

    nvim tree width 50

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -28,7 +28,7 @@ return {
     require('nvim-tree').setup {
       sort_by = 'case_sensitive',
       view = {
-        width = 30,
+        width = 50,
         preserve_window_proportions = true,
       },
       filters = {
