1dcf6b252b6d4292fb9ced194a53f9d5c12a3d02
Allen Chen <alchen@drwholdings.com>
2025-04-29 14:50:29 -0500

    update

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -13,6 +13,25 @@ return { -- Highlight, edit, and navigate code
     },
   },
   {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'BufReadPre',
+    config = function()
+      require('treesitter-context').setup {
+        enable = true,
+        multiwindow = true,
+        max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 2, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer',
+        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil,
+      }
+    end,
+  },
+  {
     'nvim-treesitter/nvim-treesitter-textobjects',
     event = 'BufReadPre',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
