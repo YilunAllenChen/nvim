de4c6b445165cf21c70293965b9df97954735a94
Allen Chen <alchen@drwholdings.com>
2025-05-06 09:51:55 -0500

    fix avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -6,10 +6,10 @@ return {
     provider = 'azure',
     azure = {
       endpoint = 'https://azure-openai.drwcloud.com',
-      model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+      deployment = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
       temperature = 0,
-      -- max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      max_completion_tokens = 4096, -- Increase this to include reasoning tokens (for reasoning models)
       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
     },
   },
