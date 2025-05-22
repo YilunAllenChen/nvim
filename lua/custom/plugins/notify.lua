22d71e6166738a785e3522a055818b188e5c7c4a
Allen Chen <alchen@drwholdings.com>
2025-05-22 11:39:45 -0500

    revive notify

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,15 +1,15 @@
 return {
-  -- 'rcarriga/nvim-notify',
-  -- event = 'VeryLazy',
-  -- config = function()
-  --   require('notify').setup {
-  --     merge_duplicates = false,
-  --     level = vim.log.levels.WARN,
-  --     stages = 'slide',
-  --     timeout = 3000,
-  --     render = 'wrapped-compact',
-  --     top_down = false,
-  --     fps = 60,
-  --   }
-  -- end,
+  'rcarriga/nvim-notify',
+  event = 'VeryLazy',
+  config = function()
+    require('notify').setup {
+      merge_duplicates = false,
+      level = vim.log.levels.WARN,
+      stages = 'slide',
+      timeout = 3000,
+      render = 'wrapped-compact',
+      top_down = false,
+      fps = 60,
+    }
+  end,
 }
