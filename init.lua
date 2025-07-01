73a406ddfec42b106c8f6817ea618d5b7ab69c71
Allen Chen <alchen@drwholdings.com>
2025-07-01 07:53:04 -0500

    neovide guifont

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ end
 
 if vim.g.neovide then
   vim.g.neovide_scale_factor = 1.2
+  vim.o.guifont = 'FiraCode Nerd Font:h14'
   vim.g.neovide_position_animation_length = 0.0
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_transparency = 0.9
