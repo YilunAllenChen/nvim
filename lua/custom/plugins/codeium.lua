26ba0637c7d431f8f73a3e26557f1ba657041bd2
Allen Chen <alchen@drwholdings.com>
2025-09-21 22:11:38 -0500

    cleanup

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,6 +1,5 @@
 return {
   'Exafunction/codeium.vim',
-  -- tag = '1.4.15',
   event = 'BufEnter',
   config = function()
     vim.keymap.set('i', '<Tab>', function()
--- lua/custom/plugins/yanky.lua
+++ lua/custom/plugins/yanky.lua
@@ -4,13 +4,13 @@ return {
     ring = { history_length = 20 },
     picker = { telescope = { use_default_mappings = false } },
     system_clipboard = {
-      sync_with_ring = true,
+      sync_with_ring = false,
       clipboard_register = nil,
     },
     highlight = {
       on_put = true,
       on_yank = true,
-      timer = 500,
+      timer = 100,
     },
     preserve_cursor_position = {
       enabled = true,
