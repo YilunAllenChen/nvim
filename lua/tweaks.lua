2dc6e7ef8c1f66d49ed03be039b58de7d91c72c5
Allen Chen <alchen@drwholdings.com>
2025-09-25 20:51:25 -0500

    try clipboard magic

--- lua/tweaks.lua
+++ lua/tweaks.lua
@@ -1,9 +1,7 @@
 -- Shell & GUI tweaks
 
 -- Prefer fish as shell when available
-if vim.fn.executable 'fish' == 1 then
-  vim.opt.shell = 'fish'
-end
+if vim.fn.executable 'fish' == 1 then vim.opt.shell = 'fish' end
 
 -- Neovide GUI settings
 if vim.g.neovide then
@@ -12,4 +10,24 @@ if vim.g.neovide then
   vim.g.neovide_position_animation_length = 0.0
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_transparency = 0.9
+end
+
+local opt = vim.opt
+opt.clipboard = 'unnamedplus'
+if vim.env.SSH_CONNECTION then
+  local function vim_paste()
+    local content = vim.fn.getreg '"'
+    return vim.split(content, '\n')
+  end
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = vim_paste,
+      ['*'] = vim_paste,
+    },
+  }
 end
