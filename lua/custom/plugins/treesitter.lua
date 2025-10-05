4e6e912147d0fc796bf1717182a03f6f2d76eb86
Allen Chen <alchen@drwholdings.com>
2025-10-04 21:08:45 -0500

    dont use ts incremental selection

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -39,13 +39,7 @@ return { -- Highlight, edit, and navigate code
     config = function()
       require('nvim-treesitter.configs').setup {
         incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<CR>',
-            scope_incremental = '<C-CR>',
-            node_incremental = '<CR>',
-            node_decremental = '<BS>',
-          },
+          enable = false,
         },
         textobjects = {
           select = {
