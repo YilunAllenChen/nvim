b7b80d4a06d914ff7404f89c297aedbf86835904
Allen Chen <alchen@drwholdings.com>
2025-10-14 07:27:29 -0500

    dial

--- lua/custom/plugins/dial.lua
+++ lua/custom/plugins/dial.lua
@@ -2,10 +2,10 @@ return {
   'monaqa/dial.nvim',
   event = 'InsertEnter',
   config = function()
-    local config = require('dial.config')
-    local augend = require('dial.augend')
+    local config = require 'dial.config'
+    local augend = require 'dial.augend'
 
-    local default = config.augends:get('default')
+    local default = config.augends:get 'default'
     local has_bool = false
 
     for _, entry in ipairs(default) do
@@ -15,9 +15,7 @@ return {
       end
     end
 
-    if not has_bool then
-      table.insert(default, augend.constant.alias.bool)
-    end
+    if not has_bool then table.insert(default, augend.constant.alias.bool) end
   end,
   keys = {
     { '<C-a>', function() require('dial.map').manipulate('increment', 'normal') end, desc = 'Increment' },
