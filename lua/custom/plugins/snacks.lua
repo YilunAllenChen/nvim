247215efc86320c53d257ce73f87f33c0dd0b77c
Allen Chen <alchen@drwholdings.com>
2026-01-21 07:33:39 -0600

    main

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -26,6 +26,7 @@ return {
     },
     picker = {
       enabled = true,
+      main = { file = false },
       win = {
         input = {
           keys = {
@@ -69,7 +70,7 @@ return {
   },
   keys = {
     -- -- Top Pickers & Explorer
-    { '=', function() require('snacks').picker.files({ main = { file = false } }) end, desc = 'Find Files' },
+    { '=', function() require('snacks').picker.files() end, desc = 'Find Files' },
     { ',', function() require('snacks').picker.grep() end, desc = 'Grep' },
     -- -- find
     { '<leader>j', function() require('snacks').picker.buffers() end, desc = 'Buffers' },
