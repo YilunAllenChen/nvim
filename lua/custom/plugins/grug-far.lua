1e8fdd1ea18b573cb0e9f50a321194996cbee8db
Allen Chen <alchen@drwholdings.com>
2026-02-28 09:23:34 -0600

    cleanup

--- lua/custom/plugins/grug-far.lua
+++ lua/custom/plugins/grug-far.lua
@@ -1,13 +1,7 @@
 return {
   'MagicDuck/grug-far.nvim',
   event = 'VeryLazy',
-  config = function()
-    require('grug-far').setup {
-      -- options, see Configuration section below
-      -- there are no required options atm
-      -- engine = 'ripgrep' is default, but 'astgrep' can be specified
-    }
-  end,
+  config = function() require('grug-far').setup {} end,
   keys = {
     { '<leader>r', '<cmd>GrugFar<cr>', desc = 'Grug search & replace' },
   },
