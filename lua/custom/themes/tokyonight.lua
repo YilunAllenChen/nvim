9e1212167060edc88e39a3435b6141d05242c60f
Allen Chen <alchen@drwholdings.com>
2026-07-15 09:43:12 -0500

    divider

--- lua/custom/themes/tokyonight.lua
+++ lua/custom/themes/tokyonight.lua
@@ -1,5 +1,13 @@
 return {
   'folke/tokyonight.nvim',
   priority = 1000,
-  config = function() vim.cmd.colorscheme 'tokyonight-moon' end,
+  config = function()
+    require('tokyonight').setup {
+      style = 'moon',
+      dim_inactive = true, -- darken inactive splits so active window stands out
+      on_highlights = function(hl, c) hl.WinSeparator = { fg = c.blue, bold = true } end, -- brighter split divider
+    }
+    vim.cmd.colorscheme 'tokyonight-moon'
+    vim.opt.fillchars:append { vert = '│', horiz = '─', horizup = '┴', horizdown = '┬', vertleft = '┤', vertright = '├', verthoriz = '┼' }
+  end,
 }
